56400ae81baa26b1a2c898c65e9976158b8559ad
Sam Chun <sanghun89@gmail.com>
2022-09-05 23:47:43 -0500

    convert over to lua

---
+++ config/.config/nvim/init.lua
@@ -0,0 +1,17 @@
+require('tribecalledtest.base')
+require('tribecalledtest.highlights')
+require('tribecalledtest.maps')
+require('tribecalledtest.plugins')
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('tribecalledtest.macos')
+end
+if is_win then
+  require('tribecalledtest.windows')
+end
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -102,3 +102,5 @@ source $ZSH/oh-my-zsh.sh
 
 source ~/.zsh_profile
 
+alias vim="nvim"
+
