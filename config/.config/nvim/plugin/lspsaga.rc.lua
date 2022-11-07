2051e7c8d390249dcbb7e499c1bbd45f84fa5a39
Sam Chun <sanghun89@gmail.com>
2022-11-06 23:56:29 -0600

    fix set

--- config/.config/nvim/plugin/lspsaga.rc.lua
+++ config/.config/nvim/plugin/lspsaga.rc.lua
@@ -8,15 +8,15 @@ saga.init_lsp_saga {
 }
 
 local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_buf_set_keymap
+local set = vim.keymap.set
 
-map(0, "n", "gr", "<cmd>Lspsaga rename<cr>", {silent = true, noremap = true})
-map(0, "n", "gx", "<cmd>Lspsaga code_action<cr>", {silent = true, noremap = true})
-map(0, "x", "gx", ":<c-u>Lspsaga range_code_action<cr>", {silent = true, noremap = true})
-map(0, "n", "K",  "<cmd>Lspsaga hover_doc<cr>", {silent = true, noremap = true})
-map(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true})
-map(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true})
-map(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true})
-map(0, "n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<cr>", {})
-map(0, "n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>", {})
+set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+set("n", "gx", "<Cmd>Lspsaga code_action<CR>", opts)
+set("x", "gx", ":<Cmd>Lspsaga range_code_action<CR>", opts)
+set("n", "K",  "<Cmd>Lspsaga hover_doc<CR>", opts)
+set("n", "go", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
+set("n", "gj", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+set("n", "gk", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+set("n", "<C-u>", "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-u>')<CR>", {})
+set("n", "<C-d>", "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<CR>", {})
 
