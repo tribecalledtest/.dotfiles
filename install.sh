ba1d67ad5d2e320dd8277f5f708814e964e8777b
Sam Chun <sanghun89@gmail.com>
2023-02-15 22:54:07 -0600

    gnu and idea

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-STOW_FOLDERS="config,git,local,tmux,yabai,zsh,bazel,nvm,dev"
+STOW_FOLDERS="config,git,local,tmux,yabai,zsh,bazel,nvm,dev,idea,gnupg"
 DOTFILES=$HOME/.dotfiles
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
