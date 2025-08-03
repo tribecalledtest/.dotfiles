71ba720dd4b70918f9b580ad95dad9900375c744
Sam Chun <sanghun89@gmail.com>
2025-08-02 23:07:35 -0500

    meh

--- config/.config/nvim/after/plugin/lsp.lua
+++ config/.config/nvim/after/plugin/lsp.lua
@@ -58,7 +58,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { 'ts_ls', 'rust_analyzer' },
+    ensure_installed = { 'ts_ls', 'rust_analyzer', 'clangd' },
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
--- config/.config/nvim/lua/tribecalledtest/packer.lua
+++ config/.config/nvim/lua/tribecalledtest/packer.lua
@@ -8,14 +8,14 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use({
         'rose-pine/neovim',
-        as = 'rose-pine',
+        name = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
         end
